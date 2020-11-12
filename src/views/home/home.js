3f6a159b20637a8d9d31e07186663804a6219b25
michelluiss <michelluis88@gmail.com>
2020-11-11 23:35:00 -0300

    exibindo texto na page home

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Home = () => (
-  <div className="home">
-    <h1>Home</h1>
-  </div>
+  <section className="page-home">
+    <div className="container">
+      <div className="content-page-home">
+        <p>we are <strong>square</strong></p>
+        <p>and we are <strong>coming soon</strong></p>
+      </div>
+    </div>
+  </section>
 )
 
 export default Home;
