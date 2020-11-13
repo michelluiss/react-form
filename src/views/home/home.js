77423dddd8a45d8a6296dbb5178fea83b73579f4
michelluiss <michelluis88@gmail.com>
2020-11-13 14:58:31 -0300

    responsivo pagina contato

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import CountTimer from '../../components/CountTimer'
+import bgSquare from '../../assets/image/bg-img.png';
 
 const Home = () => (
   <section className="page-home">
+    <img src={bgSquare} alt="imagem de background" />
     <div className="container">
       <div className="content-page-home">
         <p>we are <strong>square</strong></p>
