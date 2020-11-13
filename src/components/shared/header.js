1ad74b3ef4c43f4b406d42bcbe8be38fae594672
michelluiss <michelluis88@gmail.com>
2020-11-12 21:19:13 -0300

    página contact, estrutura e estilo parte 1

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <div className="container">
+  <div>
     <header className="header-app">
       <div className="logo">
       </div>
--- src/views/contact/contact.js
+++ src/views/contact/contact.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { GrFacebook } from "react-icons/gr";
+import { GrGooglePlus } from "react-icons/gr";
+import { GrLinkedin } from "react-icons/gr";
+import { FaTwitter } from "react-icons/fa";
 
 const Contact = () => (
-  <div>
-    <h1>Contacts</h1>
-  </div>
+  <section className="page-contact">
+    <div className="west-side">
+      <div className="container">
+        <h1>Contact <strong>Us</strong></h1>
+        <div className="content-text">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+        <div className="content-info">
+          <p><strong>Adress:</strong>123 Palo Alto, Palo Alto, CA 94300</p>
+          <p><strong>Phone:</strong>+1 234-567-8910</p>
+          <p><strong>E-mail:</strong>contact@yourdomain.net</p>
+        </div>
+        <div className="content-icons">
+          <div className="box-icon">
+            <GrFacebook color='#fff' size='30'></GrFacebook>
+          </div>
+          <div className="box-icon bg-icons">
+            <GrGooglePlus color='#ea3d3d' size='20'></GrGooglePlus>
+          </div>
+          <div className="box-icon">
+            <GrLinkedin color='#fff' size='30'></GrLinkedin>
+          </div>
+          <div className="box-icon bg-icons">
+            <FaTwitter color='#ea3d3d' size='20'></FaTwitter>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="east-side">
+      <div className="container">
+      </div>
+    </div>
+  </section>
 )
 
 export default Contact;
